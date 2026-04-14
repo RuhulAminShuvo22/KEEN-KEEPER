fb539e928d03ecce4fcb12e32ca3b71eba408c07
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-14 23:47:34 +0600

    complete update on navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import logo from "../assets/logo.png";
 
 export default function Navbar() {
 
@@ -11,13 +10,10 @@ export default function Navbar() {
   return (
     <div className="w-full bg-gray-100 border-b border-gray-200 px-6 py-3 flex items-center justify-between">
 
-      {/* Logo */}
-      <div className="flex items-center gap-2">
-        <img src={logo} className="w-7 h-7" />
-        <h1 className="font-semibold text-lg text-gray-800">
-          Keen<span className="text-green-700">Keeper</span>
-        </h1>
-      </div>
+      {/* Logo Text Only */}
+      <h1 className="font-semibold text-lg text-gray-800">
+        Keen<span className="text-green-700">Keeper</span>
+      </h1>
 
       {/* Desktop Menu */}
       <div className="hidden md:flex items-center gap-3">
@@ -43,11 +39,9 @@ export default function Navbar() {
         </label>
 
         <ul className="menu menu-sm dropdown-content mt-3 right-0 p-2 shadow bg-white rounded-lg w-44">
-
           <li><NavLink to="/">🏠 Home</NavLink></li>
           <li><NavLink to="/timeline">🕒 Timeline</NavLink></li>
           <li><NavLink to="/stats">📈 Stats</NavLink></li>
-
         </ul>
       </div>
 
