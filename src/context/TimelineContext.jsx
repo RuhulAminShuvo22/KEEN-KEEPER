75dd5fc446a74045f284679abcd71ff0e005e3a5
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-14 14:23:04 +0600

    basic setup for timeline controller

---
+++ src/context/TimelineContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export const TimelineContext = createContext();
+
+export const TimelineProvider = ({ children }) => {
+  const [timeline, setTimeline] = useState([]);
+
+  const addEntry = (type, name) => {
+    const entry = {
+      date: new Date().toLocaleDateString(),
+      title: `${type} with ${name}`,
+      type,
+    };
+    setTimeline(prev => [entry, ...prev]);
+  };
+
+  return (
+    <TimelineContext.Provider value={{ timeline, addEntry }}>
+      {children}
+    </TimelineContext.Provider>
+  );
+};
