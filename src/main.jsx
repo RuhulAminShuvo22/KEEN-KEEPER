7285959232e61e5494d40320fdee5c3efe190ff9
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-14 14:52:53 +0600

    main section little part

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+// ✅ Context
+import { TimelineProvider } from "./context/TimelineContext";
+
+// ✅ Toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
