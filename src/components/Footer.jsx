c99872845a588f1a4d78a978f72a2ccd38dc0ab8
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-15 00:00:42 +0600

    some fiture added on footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,15 +25,27 @@ export default function Footer() {
 
           <div className="flex justify-center gap-4">
 
-            <div className="bg-white p-2 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer">
+            {/* Facebook */}
+            <div className="bg-white p-2 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer 
+            transition duration-300 ease-in-out
+            hover:scale-110 hover:-translate-y-1 
+            hover:shadow-[0_0_20px_rgba(255,255,255,0.7)]">
               <img src={facebook} className="w-5" />
             </div>
 
-            <div className="bg-white p-2 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer">
+            {/* Instagram */}
+            <div className="bg-white p-2 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer 
+            transition duration-300 ease-in-out
+            hover:scale-110 hover:-translate-y-1 
+            hover:shadow-[0_0_20px_rgba(255,255,255,0.7)]">
               <img src={instagram} className="w-5" />
             </div>
 
-            <div className="bg-white p-2 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer">
+            {/* Twitter */}
+            <div className="bg-white p-2 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer 
+            transition duration-300 ease-in-out
+            hover:scale-110 hover:-translate-y-1 
+            hover:shadow-[0_0_20px_rgba(255,255,255,0.7)]">
               <img src={twitter} className="w-5" />
             </div>
 
@@ -44,12 +56,14 @@ export default function Footer() {
       {/* BOTTOM BAR */}
       <div className="bg-[#295c4a] border-t border-white/10 text-gray-300 text-sm px-6 py-4 flex flex-col md:flex-row justify-between items-center">
 
+        {/* Left */}
         <p>© 2026 KeenKeeper. All rights reserved.</p>
 
+        {/* Right */}
         <div className="flex gap-6 mt-2 md:mt-0">
-          <span className="cursor-pointer hover:text-white">Privacy Policy</span>
-          <span className="cursor-pointer hover:text-white">Terms of Service</span>
-          <span className="cursor-pointer hover:text-white">Cookies</span>
+          <span className="cursor-pointer hover:text-white transition">Privacy Policy</span>
+          <span className="cursor-pointer hover:text-white transition">Terms of Service</span>
+          <span className="cursor-pointer hover:text-white transition">Cookies</span>
         </div>
 
       </div>
